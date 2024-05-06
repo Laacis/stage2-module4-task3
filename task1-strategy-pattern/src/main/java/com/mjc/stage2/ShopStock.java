3a4c9b1a87f1e1163b7c0898db10bd0c37cc89b0
Laacis <109668309+Laacis@users.noreply.github.com>
2024-05-06 17:20:14 +0800

    implemented ShopStock

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,13 +1,17 @@
 package com.mjc.stage2;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShopStock {
     private final List<Product> productList;
+    private FilteringStrategy filteringStrategy;
 
     public ShopStock(List<Product> productList) {
         this.productList = productList;
     }
 
-    // Write your code here!
+    public  List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy) {
+        return productList.stream().filter(filteringStrategy::filter).collect(Collectors.toList());
+    };
 }
