c59ffcf7ca601c8b2efe9e2b0324fc238c23ef54
Laacis <109668309+Laacis@users.noreply.github.com>
2024-05-07 15:18:26 +0800

    implemented SymboilLeaf

--- task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
+++ task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
@@ -1,8 +1,31 @@
 package com.mjc.stage2.entity;
 
-public class SymbolLeaf {
+public class SymbolLeaf extends AbstractTextComponent{
 
     private char value;
 
-    // Write your code here!
+    public SymbolLeaf(char c, TextComponentType componentType) {
+        super(componentType);
+        this.value = c;
+    }
+
+    @Override
+    public String operation() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public void add(AbstractTextComponent textComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(AbstractTextComponent textComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException();
+    }
 }
