c3daefa4dcfab7eda7291fc86aa2316c00c69474
Laacis <109668309+Laacis@users.noreply.github.com>
2024-05-08 23:30:56 +0800

    add Unittest to test parsing

--- task3-handling/src/test/java/com/mjc/stage2/parser/ChainParserBuilderTest.java
+++ task3-handling/src/test/java/com/mjc/stage2/parser/ChainParserBuilderTest.java
@@ -1,5 +1,8 @@
 package com.mjc.stage2.parser;
 
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.TextComponent;
+import com.mjc.stage2.entity.TextComponentType;
 import org.junit.jupiter.api.Test;
 
 public class ChainParserBuilderTest {
@@ -13,5 +16,22 @@ public class ChainParserBuilderTest {
 
         AbstractTextParser result = builder.build();
         assert  result.nextParser.getClass() == WordParser.class : "Expected another AbstractTextParserClass";
+    }
+
+    @Test
+    public void ChainParserBuilderBuildBuildAndParse() {
+        ChainParserBuilder builder = new ChainParserBuilder();
+
+        builder.setParser(new LexemeParser());
+        builder.setParser(new WordParser());
+
+        AbstractTextParser parser = builder.build();
+        String testText = "Sometimes, it's useful to share arguments between different test classes.";
+        AbstractTextComponent text = new TextComponent(TextComponentType.SENTENCE);
+
+        parser.parse(text, testText);
+        String result = text.operation();
+        assert  result.matches(testText) : "result text is not as expected, result: '" + result + "' but was expected :" +testText;
+
     }
 }
