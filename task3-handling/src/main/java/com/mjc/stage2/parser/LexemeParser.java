7a471c4dd48e37b71646a00c6ef598c8af69b979
Laacis <109668309+Laacis@users.noreply.github.com>
2024-05-08 19:46:09 +0800

    implemented LexemeParser

--- task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
@@ -1,9 +1,22 @@
 package com.mjc.stage2.parser;
 
-public class LexemeParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.TextComponent;
+import com.mjc.stage2.entity.TextComponentType;
+
+
+public class LexemeParser extends AbstractTextParser{
     private static final String LEXEME_REGEX = "\\s+";
     private static final String WORD_REGEX = "\\w[\\w!=?():]+";
 
-    // Write your code here!
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        String[] lexemes = string.split(LEXEME_REGEX);
+        for (String lexeme : lexemes){
+            if (lexeme.matches(WORD_REGEX)) {
+                abstractTextComponent.add(new TextComponent(TextComponentType.WORD));
+            }
 
+        }
+    }
 }
